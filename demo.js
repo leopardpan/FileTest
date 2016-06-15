15043c7014e610cbc5ec95b67da35e33d8a0be03
leopardpan <panbaixin123@163.com>
2016-06-15 12:01:54 +0800

    修改脚本

--- demo.js
+++ demo.js
@@ -1,6 +1,9 @@
-defineClass('MineViewController', {
-  viewDidLoad: function() {
-    console.log('JSPatch test...')
-    self.ORIGviewDidLoad()
-  }
+defineClass('AppDelegate', {
+	require('TalkingData,Bugly');
+	setupAnalytics: function() {
+	    TalkingData.setExceptionReportEnabled(YES);
+	    TalkingData.sessionStarted_withChannelId("C08C1943294887A2104FF41FA4BA68DD", "fir");
+
+	    Bugly.startWithAppId("900033953");
+	}
 })
