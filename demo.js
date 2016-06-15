40fb07db77ebddd568a34e65dc81610275339a9d
leopardpan <panbaixin123@163.com>
2016-06-15 12:08:09 +0800

    dix patch

--- demo.js
+++ demo.js
@@ -1,5 +1,6 @@
+require('TalkingData,Bugly');
 defineClass('AppDelegate', {
-	require('TalkingData,Bugly');
+	
 	setupAnalytics: function() {
 	    TalkingData.setExceptionReportEnabled(YES);
 	    TalkingData.sessionStarted_withChannelId("C08C1943294887A2104FF41FA4BA68DD", "fir");
